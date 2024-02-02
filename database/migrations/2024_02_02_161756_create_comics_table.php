96aaa65f123d2f4ec4a97a28fe983440c174d129
vitodursopettersson <vitodurso98@gmail.com>
2024-02-02 20:33:31 +0100

    feat: migration create comics table

---
+++ database/migrations/2024_02_02_161756_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('thumb');
+            $table->decimal('price', 8, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->text('artists');
+            $table->text('writers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
